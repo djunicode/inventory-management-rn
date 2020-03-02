2db9db5d70d0638904ccfb2f59a79a8ae22b6e01
Tejas Ghone <tejasghone73@gmail.com>
2020-03-02 21:34:25 +0530

    Login integration with backend done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, Button} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-
-function Home() {
-  return(
+function Home({navigation}) {
+  async function Logout() {
+    // console.log(await AsyncStorage.getItem('auth_key'));
+    await AsyncStorage.removeItem('auth_key');
+    // console.log(await AsyncStorage.getItem('auth_key'));
+    navigation.navigate('LoginScreen');
+  }
+  return (
     <View>
       <Text>This is Home.js</Text>
+      <Button
+        title="Logout"
+        onPress={() => {
+          Logout();
+        }}></Button>
     </View>
   );
 }
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,14 @@
-
-import React, { Component } from 'react';
-import { Button, Body, Input, Container, Content, Header, Item, Label, } from 'native-base';
+import React, {Component, useState} from 'react';
+import {
+  Button,
+  Body,
+  Input,
+  Container,
+  Content,
+  Header,
+  Item,
+  Label,
+} from 'native-base';
 import {
   SafeAreaView,
   StyleSheet,
@@ -12,59 +20,119 @@ import {
   KeyboardAvoidingView,
   Dimensions,
 } from 'react-native';
+import Axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
 
-function LoginScreen({ navigation }) {
+async function getLoginToken(email, password) {
+  const response = await Axios.post(
+    'http://chouhanaryan.pythonanywhere.com/auth/token/login',
+    {
+      password: password,
+      email: email,
+    },
+  );
+  const token = await response.data;
+  return token.auth_token;
+}
+async function Login(navigation, email, password) {
+  if (email !== '' && password !== '') {
+    try {
+      await AsyncStorage.setItem(
+        'auth_key',
+        await getLoginToken(email, password),
+      );
+    } catch (e) {
+      console.log(e);
+    }
+
+    navigation.navigate('HomeScreen');
+  }
+}
+
+function LoginScreen({navigation}) {
+  async function LoginCheck() {
+    console.log(await AsyncStorage.getItem('auth_key'));
+    if ((await AsyncStorage.getItem('auth_key')) !== null) {
+      navigation.navigate('HomeScreen');
+    }
+  }
+  LoginCheck();
+  const [email, setUserEmail] = useState('');
+  const [password, setUserPassword] = useState('');
+
+  const handlechange_email = value => {
+    setUserEmail(value);
+  };
+  const handlechange_password = value => {
+    setUserPassword(value);
+  };
 
   return (
-    <Container style={{ backgroundColor: '#4fc3fa' }}>
+    <Container style={{backgroundColor: '#4fc3fa'}}>
       <Content>
         <ScrollView>
-
           <Body>
             <Text style={styles.heading}>Login</Text>
-            
-            <Text style={{ padding: 80, borderWidth: 1, borderColor: '#000' }}>[Image will come here]</Text>
-            
+
+            <Text style={{padding: 80, borderWidth: 1, borderColor: '#000'}}>
+              [Image will come here]
+            </Text>
+
             <Item floatingLabel style={styles.inputBox}>
               <Label style={styles.label}>Email ID</Label>
-              <Input keyboardType='email-address' autoCapitalize='none' />
+              <Input
+                onChangeText={value => {
+                  handlechange_email(value);
+                }}
+                name="email"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                // value={userInput.email}
+              />
             </Item>
-            
+
             <Item floatingLabel style={styles.inputBox}>
               <Label style={styles.label}>Password</Label>
-              <Input secureTextEntry />
+              <Input
+                onChangeText={value => {
+                  handlechange_password(value);
+                }}
+                name="password"
+                secureTextEntry
+                // value={userInput.password}
+              />
             </Item>
-            
-            <TouchableOpacity rounded style={styles.loginButton} onPress={() => { navigation.navigate('HomeScreen') }} >
-              <Text style={styles.buttonText} >Login</Text>
+
+            <TouchableOpacity
+              rounded
+              style={styles.loginButton}
+              onPress={() => {
+                Login(navigation, email, password);
+              }}>
+              <Text style={styles.buttonText}>Login</Text>
             </TouchableOpacity>
-            
-            <View style={{ flexDirection: 'row', marginTop: 10 }}>
-              <View style={{ marginRight: 4, }}><Text>New User?</Text></View>
-              <View style={{ marginLeft: 4, }}>
-                <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+
+            <View style={{flexDirection: 'row', marginTop: 10}}>
+              <View style={{marginRight: 4}}>
+                <Text>New User?</Text>
+              </View>
+              <View style={{marginLeft: 4}}>
+                <TouchableOpacity
+                  onPress={() => navigation.navigate('Register')}>
                   <Text style={styles.register}>Register</Text>
                 </TouchableOpacity>
               </View>
             </View>
-          
           </Body>
         </ScrollView>
-
       </Content>
     </Container>
   );
-
 }
-
-
-
-
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-
   loginButton: {
     width: 280,
     height: 40,
@@ -111,7 +179,6 @@ const styles = StyleSheet.create({
   },
   label: {
     marginTop: 5,
-    marginBottom: 10
-  }
-
-})
+    marginBottom: 10,
+  },
+});
