0aa580ab672c29e1bab46379ace75aa1c2a06b67
NamanLad <namanlad@gmail.com>
2020-04-10 17:34:50 +0530

    New input fields visible on Add product button press in buy and sell pages

--- src/components/Sell.js
+++ src/components/Sell.js
@@ -1,45 +1,70 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 import { Container, Content, Tab, Tabs, Header, Left, Right, Body, Item, Input, Label } from 'native-base';
 import Icon from 'react-native-vector-icons/Feather';
 
 const Buy = ({ navigation }) => {
+
+  const [product, setProduct] = useState([{ name: '', price: 0, amount: 0 }]);
+
   return (
     <Container style={{ backgroundColor: '#F3F9FB' }}>
       <Content>
         <Body>
           <Text style={styles.heading}>Sell Items</Text>
 
-          <Item floatingLabel style={styles.inputBox}>
-            <Label style={styles.label}>Product Name</Label>
-            <Input
-              style={styles.inputArea}
-            />
-          </Item>
+          {product.map((item, index) => {
+            let copy = [...product]
+            return (
+              <View key={index} style={{ width: Dimensions.get('window').width }}>
+                {/* for the separating line */}
+                <View style={{ borderColor: '#0004', borderWidth: 1, width: '50%', alignSelf: 'center', borderRadius: 2, marginBottom: -10, marginTop: 5 }} />
 
-          <Item floatingLabel style={styles.inputBox}>
-            <Label style={styles.label}>Price</Label>
-            <Input
-              style={styles.inputArea}
-              keyboardType='numeric'
-            />
-          </Item>
+                <Text style={styles.product_titles}>Product {product.length == 1 ? '' : index + 1}</Text>
+                <Item floatingLabel style={styles.inputBox}>
+                  <Label style={styles.label}>Product Name</Label>
+                  <Input
+                    style={styles.inputArea}
+                    onChangeText={(value) => copy[index].name = value}
+                  />
+                </Item>
 
-          <Item floatingLabel style={styles.inputBox}>
-            <Label style={styles.label}>No. of Items</Label>
-            <Input
-              style={styles.inputArea}
-              keyboardType='numeric'
-            />
-          </Item>
+                <Item floatingLabel style={styles.inputBox}>
+                  <Label style={styles.label}>Price</Label>
+                  <Input
+                    style={styles.inputArea}
+                    keyboardType='numeric'
+                    onChangeText={(value) => copy[index].price = value}
+                  />
+                </Item>
+
+                <Item floatingLabel style={styles.inputBox}>
+                  <Label style={styles.label}>No. of Items</Label>
+                  <Input
+                    style={styles.inputArea}
+                    keyboardType='numeric'
+                    onChangeText={(value) => copy[index].amount = value}
+                  />
+                </Item>
+              </View>
+            )
+          })}
 
           <TouchableOpacity
+onPress={() => {
+              let copy = [...product];
+              copy.push({ name: '', price: 0, amount: 0 })
+              setProduct(copy)
+            }}
             style={styles.addButton}>
             <Icon name="plus" color="#4796BD" size={25} style={styles.icon} />
             <Text style={styles.addButtonText}>Add Product</Text>
           </TouchableOpacity>
 
           <TouchableOpacity
+          onPress={() => { 
+              console.log(product)
+             }}
             style={styles.sellButton}>
             <Text style={styles.sellButtonText}>Sell</Text>
           </TouchableOpacity>
@@ -59,6 +84,14 @@ const styles = StyleSheet.create({
     fontSize: 26,
     color: '#122E40',
     fontWeight: 'bold',
+    marginTop: 25,
+    marginBottom: 10,
+    alignSelf: 'center',
+    marginLeft: '5%'
+  },
+  product_titles: {
+    fontSize: 24,
+    color: '#122E40',
     marginTop: 25,
     marginBottom: 10,
     alignSelf: 'flex-start',
