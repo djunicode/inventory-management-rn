904c4cf496b5b70582cdeff95dcac390066d5a8f
jinit <jinit2000@yahoo.com>
2020-05-07 21:30:35 +0530

    sell remaining

--- src/components/History.js
+++ src/components/History.js
@@ -1,9 +1,31 @@
-import React, { Component ,useState, useEffect} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 import {
-    Button, Body, Input, Container, Content, Header, Right, Left, Item, Label, Card, CardItem,} from 'native-base';
-import {Alert, FlatList, SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, TouchableOpacity,
-    KeyboardAvoidingView, Dimensions,
+  Button,
+  Body,
+  Input,
+  Container,
+  Content,
+  Header,
+  Right,
+  Left,
+  Item,
+  Label,
+  Card,
+  CardItem,
+} from 'native-base';
+import {
+  Alert,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Dimensions,
 } from 'react-native';
 import HistoryListItem from '../components/HistoryListItem';
 import HeaderView from '../components/HeaderView';
@@ -24,160 +46,110 @@ const DEMO_HISTORY_DATA = [
         product: 'Pat Black',
         items: 20,
         price: '₹45',
-    },
-    {
-        id: '3',
-        date: '23 Dec',
-        type: 'Buy',
-        product: 'Pat Black',
-        items: 20,
-        price: '₹45',
-    },
-    {
-        id: '4',
-        date: '23 Dec',
-        type: 'Buy',
-        product: 'Pat Black',
-        items: 20,
-        price: '₹45',
-    },
-    {
-        id: '5',
-        date: '23 Dec',
-        type: 'Buy',
-        product: 'Pat Black',
-        items: 20,
-        price: '₹45',
-    },
-    {
-        id: '6',
-        date: '23 Dec',
-        type: 'Buy',
-        product: 'Pat Black',
-        items: 20,
-        price: '₹45',
-    },
-    {
-        id: '7',
-        date: '23 Dec',
-        type: 'Buy',
-        product: 'Pat Black',
-        items: 20,
-        price: '₹45',
-    },
-    {
-        id: '8',
-        date: '23 Dec',
-        type: 'Buy',
-        product: 'Pat Black',
-        items: 20,
-        price: '₹45',
-    },
-    {
-        id: '9',
-        date: '23 Dec',
-        type: 'Buy',
-        product: 'Pat Black',
-        items: 20,
-        price: '₹45',
-    },
-    {
-        id: '10',
-        date: '23 Dec',
-        type: 'Buy',
-        product: 'Pat Black',
-        items: 20,
-        price: '₹45',
-    },
-    {
-        id: '11',
-        date: '23 Dec',
-        type: 'Buy',
-        product: 'Pat Black',
-        items: 20,
-        price: '₹45',
-    },
-    {
-        id: '12',
-        date: '23 Dec',
-        type: 'Buy',
-        product: 'Pat Black',
-        items: 20,
-        price: '₹45',
-    },
-];
+    },]
+const HistoryScreen = ({navigation}) => {
+  
+    const [transactionList, setTransactionList] = useState([]);
 
-const HistoryScreen = ({ navigation }) => {
-    // export default class InventoryListScreen extends Component {
-    // componentDidMount() {
-    //   console.disableYellowBox = true;
-    // }
-
-    // constructor(props) {
-    //   super(props);
-    // }
-    const [transaction,setTransaction] = useState([{}]);
-useEffect(()=>{
+  useEffect(() => {
     apiFetch();
-},[]);
-    const apiFetch = async () => {
-        try {
-          const response = await axios.get('http://chouhanaryan.pythonanywhere.com/api/productlist');
-          const { data } = response;
-        //   const list = data.map(val => ({
-        //     name: val.name,
-        //     quantity: val.quantity,
-        //     price: val.latest_selling_price,
-        //     id: val.id,
-        //   }));
-       
-      console.log(data);
-        } catch (e) {
-          console.log(e);
-        }
-      };
-    
+  }, []);
+  const apiFetch = async () => {
+    try {
+      const response = await axios.get(
+        'http://chouhanaryan.pythonanywhere.com/api/bill',
+      );
+      const {data} = response;
+      //   const list = data.map(val => ({
+      //     name: val.name,
+      //     quantity: val.quantity,
+      //     price: val.latest_selling_price,
+      //     id: val.id,
+      //   }));
 
-    return (
-        <Container style={{ backgroundColor: '#F3F9FB' }}>
+      
+      setTransactionList(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const parseDate = date => {
+      const newDate =Date(date).split(' ');
 
-            <Content>
-                {/* the entire outerpart */}
-                <Body style={styles.listContainer}>
-                    {/* the header of table */}
-                    <View style={styles.tableHeader}>
-                        <CardItem style={{ backgroundColor: 'rgba(255,255,255,0)' }}>
-                            <Text style={styles.dateHeader}>Date</Text>
-                            <Text style={styles.typeHeader}>Type</Text>
-                            <Text style={styles.productHeader}>Product</Text>
-                            <Text style={styles.itemsHeader}>Items</Text>
-                            <Text style={styles.priceHeader}>Price</Text>
-                        </CardItem>
-                    </View>
+   const k=newDate[1]+' '+newDate[2];
+    return k;
+  };
 
-                    {/* the inner list */}
-                    <ScrollView>
-                        <View>
-                            <FlatList
-                                style={styles.flatlist}
-                                data={DEMO_HISTORY_DATA}
-                                // scrollEnabled={true}
-                                renderItem={({ item }) => (
-                                    <HistoryListItem
-                                        item={item}
-                                    />
-                                )}
-                                keyExtractor={item => item.id}
-                            />
-                        </View>
-                    </ScrollView>
-                
-                </Body>
-            </Content>
-        </Container>
-    );
+  // parse price to proper format
+  const parsePrice = transactions => {
+    return transactions.reduce((acc, obj) => acc + obj.rate * obj.quantity, 0);
+  };
 
-}
+  return (
+    <Container style={{backgroundColor: '#F3F9FB'}}>
+      <Content>
+        {/* the entire outerpart */}
+        <Body style={styles.listContainer}>
+          {/* the header of table */}
+          <View style={styles.tableHeader}>
+            <CardItem style={{backgroundColor: 'rgba(255,255,255,0)'}}>
+              <Text style={styles.dateHeader}>Date</Text>
+              <Text style={styles.typeHeader}>Type</Text>
+              <Text style={styles.productHeader}>Product</Text>
+              <Text style={styles.itemsHeader}>Items</Text>
+              <Text style={styles.priceHeader}>Price</Text>
+              
+            </CardItem>
+          </View>
+
+          {/* the inner list */}
+        
+          
+            <View>
+              <FlatList
+                style={styles.flatlist}
+                data={transactionList}
+                // scrollEnabled={true}
+                renderItem={(row) => (
+                    
+                  <View style={styles.container} key={(row.id )}>
+                    <CardItem style={{justifyContent:'space-evenly'}}>
+                  
+                      <Text style={styles.date}>{parseDate(row.date_time)}</Text>
+                      <Text style={styles.type}> {row.in_or_out =='In' ? 'Buy' : 'Sell'}</Text>
+                   <Text style={styles.product}>Lays,{'\n'}Lassi,{'\n'}Sanitizer
+                   {/* {row.transaction.map((val, index) =>
+    index === row.transaction.length - 1
+      ? val.name
+      : `${val.name}, `
+  )} */}
+  </Text>
+                     <Text style={styles.items}>12,{'\n'}34,{'\n'}56
+                      {/* {row.transaction.map((val, index) =>
+    index === row.transaction.length - 1
+      ? val.quantity
+      : `${val.quantity}, `
+  )} */}
+  </Text>
+  
+                       <Text style={styles.price}>
+                       1234
+                       {/* {parsePrice(row.transaction)} */}
+                       
+                       </Text> 
+                                           </CardItem>
+                  
+                  </View>
+                )}
+                keyExtractor={item => item.id}
+              />
+            </View>
+         
+        </Body>
+      </Content>
+    </Container>
+  );
+};
 
 export default HistoryScreen;
 
@@ -185,53 +157,81 @@ const DEVICE_WIDTH = Dimensions.get('screen').width;
 const DEVICE_HEIGHT = Dimensions.get('screen').height;
 
 const styles = StyleSheet.create({
-    listContainer: {
-        backgroundColor: '#fff',
-        borderColor: '#858585',
-        borderWidth: 0.5,
-        alignItems: 'center',
-        marginHorizontal: 16,
-        marginVertical: 16,
-        borderRadius: 20,
-        width: DEVICE_WIDTH - 32,
-        // flexDirection: 'column',
-        // backgroundColor: 'blue',
-    },
-    flatlist: {
-        width: DEVICE_WIDTH - 32,
-        backgroundColor: '#fff',
-        height: 600,
-        borderRadius:10,
-    },
-    tableHeader: {
-        backgroundColor: '#e7eff2',
-        width: DEVICE_WIDTH - 32,
-        borderTopRightRadius: 20,
-        borderTopLeftRadius: 20,
-    },
-    itemsHeader: {
-        flex: 0.2,
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    productHeader: {
-        flex: 0.26,
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    typeHeader: {
-        flex: 0.22,
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    dateHeader: {
-        flex: 0.22,
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    priceHeader: {
-        flex: 0.15,
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
+  container: {
+    paddingVertical: 5,
+    borderColor: '#E0E0E0',
+    borderWidth: 0.5,
+    
+  },
+  date: {
+    flex: 0.24,
+    fontSize: 16,
+    borderColor:'black',
+    
+  },
+  type: {
+    flex: 0.19,
+    fontSize: 16,
+  },
+  product: {
+    flex: 0.32,
+    fontSize: 16,
+  },
+  items: {
+    flex: 0.15,
+    fontSize: 16,
+  },
+  price: {
+    flex: 0.15,
+    fontSize: 16,
+  },
+  listContainer: {
+    backgroundColor: '#fff',
+    borderColor: '#858585',
+    borderWidth: 0.5,
+    alignItems: 'center',
+    marginHorizontal: 16,
+    marginVertical: 16,
+    borderRadius: 20,
+    width: DEVICE_WIDTH - 32,
+    // flexDirection: 'column',
+    // backgroundColor: 'blue',
+  },
+  flatlist: {
+    width: DEVICE_WIDTH - 32,
+    backgroundColor: '#fff',
+    height: 600,
+    borderRadius: 10,
+  },
+  tableHeader: {
+    backgroundColor: '#e7eff2',
+    width: DEVICE_WIDTH - 32,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+  },
+  itemsHeader: {
+    flex: 0.2,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  productHeader: {
+    flex: 0.26,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  typeHeader: {
+    flex: 0.22,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  dateHeader: {
+    flex: 0.22,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  priceHeader: {
+    flex: 0.15,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
