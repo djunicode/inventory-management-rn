0268a374c30192965024910c14046e79ac991e0e
Nirali Parekh <nirali25parekh@gmail.com>
2020-06-22 16:24:43 +0530

    profile page done

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
+import { StatusBar } from 'react-native';
 import Icon_Feather from 'react-native-vector-icons/Feather';
 import Icon_MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import {TouchableOpacity} from 'react-native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import { TouchableOpacity } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import AddEmployee from './src/screens/AddEmployee';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
@@ -12,35 +12,41 @@ import DrawerScreen2 from './src/screens/TransactionsScreen';
 import EmployeeListScreen from './src/screens/EmployeeListScreen';
 import SplashScreen from './src/screens/SplashScreen';
 import Settings from './src/screens/SettingsScreen';
+import ProfilePage from './src/screens/ProfilePage'
 import {
   NavigationContainer,
   DarkTheme,
   DefaultTheme,
 } from '@react-navigation/native';
-import {createStackNavigator, HeaderTitle} from '@react-navigation/stack';
+import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
 import RegisterScreen from './src/screens/RegisterScreen';
 const AppStack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 const EmployeeStack = createStackNavigator();
 
-const App = ({navigation}) => {
+const App = ({ navigation }) => {
   return (
     <NavigationContainer theme={MyTheme}>
       <AppStack.Navigator headerMode={null} initialRouteName="SplashScreen">
         <AppStack.Screen
           name="LoginScreen"
           component={LoginScreen}
-          options={{title: 'Inventory Management', headerTitleAlign: 'center'}}
+          options={{ title: 'Inventory Management', headerTitleAlign: 'center' }}
         />
         <AppStack.Screen
           name="SplashScreen"
           component={SplashScreen}
-          // options={{ title: 'Inventory Management', headerTitleAlign: 'center' }}
+        // options={{ title: 'Inventory Management', headerTitleAlign: 'center' }}
         />
         <AppStack.Screen
           name="RegisterScreen"
           component={RegisterScreen}
-          // options={{ title: 'Inventory Management', headerTitleAlign: 'center' }}
+        // options={{ title: 'Inventory Management', headerTitleAlign: 'center' }}
+        />
+        <AppStack.Screen
+          name="ProfilePage"
+          component={ProfilePage}
+          options={{ title: 'Employee', headerTitleAlign: 'center' }}
         />
         <AppStack.Screen
           name="Drawer"
@@ -48,14 +54,14 @@ const App = ({navigation}) => {
           options={{
             headerLeft: () => {
               return (
-                <TouchableOpacity onPress={() => {}}>
+                <TouchableOpacity onPress={() => { }}>
                   <Icon_Feather name="menu" color="white" size={35} />
                 </TouchableOpacity>
               );
             },
             headerRight: () => {
               return (
-                <TouchableOpacity onPress={() => {}}>
+                <TouchableOpacity onPress={() => { }}>
                   <Icon_Feather name="user" color="white" size={35} />
                 </TouchableOpacity>
               );
@@ -86,16 +92,17 @@ function BurgerBtn() {
         name="Home"
         component={HomeScreen}
         options={
-          ({title: 'Home'},
-          {drawerIcon: () => <Icon_MaterialIcons name="home" size={24} />})
+          ({ title: 'Home' },
+            { drawerIcon: () => <Icon_MaterialIcons name="home" size={24} /> })
         }
       />
+
       <Drawer.Screen
         name="Inventory"
         component={InventoryListScreen}
         options={
-          ({title: 'Inventory'},
-          {drawerIcon: () => <Icon_Feather name="list" size={24} />})
+          ({ title: 'Inventory' },
+            { drawerIcon: () => <Icon_Feather name="list" size={24} /> })
         }
       />
       {/* <Drawer.Screen
@@ -112,15 +119,15 @@ function BurgerBtn() {
         name="Employee"
         component={StackFn}
         options={
-          ({title: 'Employee'},
-          {drawerIcon: () => <Icon_MaterialIcons name="person" size={24} />})
+          ({ title: 'Employee' },
+            { drawerIcon: () => <Icon_MaterialIcons name="person" size={24} /> })
         }
       />
       <Drawer.Screen
         name="Transactions"
         component={DrawerScreen2}
         options={
-          ({title: 'Transactions'},
+          ({ title: 'Transactions' },
           {
             drawerIcon: () => (
               <Icon_MaterialIcons name="attach-money" size={24} />
@@ -132,8 +139,8 @@ function BurgerBtn() {
         name="Settings"
         component={Settings}
         options={
-          ({title: 'Settings'},
-          {drawerIcon: () => <Icon_MaterialIcons name="settings" size={24} />})
+          ({ title: 'Settings' },
+            { drawerIcon: () => <Icon_MaterialIcons name="settings" size={24} /> })
         }
       />
     </Drawer.Navigator>
