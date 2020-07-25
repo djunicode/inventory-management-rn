77613f2485055dfb072bd2117345eeff81a39a9e
jinit <jinit2000@yahoo.com>
2020-07-25 12:12:11 +0530

    additional dependencies

--- android/app/src/main/java/com/rn_inventory/MainApplication.java
+++ android/app/src/main/java/com/rn_inventory/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.reactnativecommunity.toolbarandroid.ReactToolbarPackage;
 import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
 import com.facebook.react.ReactNativeHost;
